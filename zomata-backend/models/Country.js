289113cd5d21c8b5ac6977bfd4fbc12a977046ef
PraneethaBalanagu <118550820+PraneethaBalanagu@users.noreply.github.com>
2025-07-02 00:13:32 +0530

    Delete zomata-backend directory

--- zomata-backend/models/Country.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const countrySchema = new mongoose.Schema({
-  country_id: Number,
-  country_name: String
-});
-
-module.exports = mongoose.model('Country', countrySchema);
